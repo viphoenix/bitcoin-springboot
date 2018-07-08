b2f8e15a46134539b0509cf76f19cb392398a520
viphoenix <phenix1991@126.com>
2018-07-08 11:53:34 +0800

    init

---
+++ src/main/java/com/phoenix/blockchain/BitcoinSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.phoenix.blockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitcoinSpringbootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/phoenix/blockchain/BitcoinSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.phoenix.blockchain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BitcoinSpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
